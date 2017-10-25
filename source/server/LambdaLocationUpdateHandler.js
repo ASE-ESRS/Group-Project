157e7b04962ca71ebc56d0b4ac94d5a3c77fc3f4
Loic Verrall <loic.verrall@gmail.com>
2017-10-25 10:26:45 +0100

    Primitive input validation and error handling added

--- source/server/LambdaLocationUpdateHandler.js
+++ source/server/LambdaLocationUpdateHandler.js
@@ -2,14 +2,36 @@ var doc = require('dynamodb-doc');
 var dynamodb = new doc.DynamoDB();
 
 exports.handler = (event, context, callback) => {
-    // Extract the three parameters from the request.
+    console.log(event.latitude);
+    // Carry out input validation on the request's parameters.
+    if (typeof event["queryStringParameters"] == 'undefined' || event["queryStringParameters"] == null) {
+        abortLocationUpdate("NO INPUT SUPPLIED", callback);
+        return;
+    }
+
+    // Extract the userId parameter.
     let userId = event["queryStringParameters"]['userId'];
+    if (userId == null || userId == "") {
+        abortLocationUpdate("NO USERID SUPPLIED", callback);
+    }
+
+    // Extract the `latitude` parameter.
     let latitude = event["queryStringParameters"]['latitude'];
+    if (latitude == null || latitude == "") {
+        abortLocationUpdate("NO LATITUDE SUPPLIED", callback);
+    }
+
+    // Extract the `longitude` parameter.
     let longitude = event["queryStringParameters"]['longitude'];
-    
+    if (longitude == null || longitude == "") {
+        abortLocationUpdate("NO LONGITUDE SUPPLIED", callback);
+    }
+
+    // At this point, we assume that the input is valid and correctly formed.
+
     // Make a note of the current time.
-    let currentDateTime = new Date().getTime().toString();
-    
+    let currentDateTime = new Date().toString();
+
     // Insert a new record into the `locations` DynamoDB table.
     dynamodb.putItem({
         "TableName" : "locations",
@@ -19,17 +41,23 @@ exports.handler = (event, context, callback) => {
             "latitude"  : { "S" : latitude },
             "longitude" : { "S" : longitude }
         }
-    }, function(err, data) {
-        if (err) {
-            context.done('error', 'inserting item into dynamodb failed: ' + err);
+    }, function(error, data) {
+        if (error) {
+            abortLocationUpdate("ERROR: INSERTING ITEM INTO DYNAMODB FAILED: " + error, callback);
         } else {
-            context.done("Weeeeee");
+            callback(null, {
+                "statusCode" : 200,
+                "headers" : { "Content-Type" : "text/html" },
+                "body" : "SUCCESSFULLY ENTERED RECORD: userId: " + userId + ", latitude: " + latitude + ", longitude: " + longitude
+            });
         }
     });
-    
+};
+
+function abortLocationUpdate(reason, callback) {
     callback(null, {
         "statusCode" : 200,
         "headers" : { "Content-Type" : "text/html" },
-        "body" : "Following entry has been inserted to DynamoDB:\n\nUser ID: " + userId + ", lat: " + latitude + ", long: " + longitude
+        "body" : reason
     });
-};
+}
